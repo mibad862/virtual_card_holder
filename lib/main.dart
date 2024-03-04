452229fa510dbe01f1a6e5764bb2050f1d88ee10
Ibad <mibad862@gmail.com>
2024-03-04 20:46:09 +0500

    added the GoRouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:virtual_card_holder/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _router = GoRouter(
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: HomePage.routeName,
+        name: HomePage.routeName,
+        builder: (context,state) => const HomePage(),
+      ),
+    ],
+  );
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
+      routerConfig: _router,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
     );
   }
 }
-
